327a8449a92ee4e205beb08c340b4be07f4f7d6c
chris frey <chris.frey@live.com>
2023-06-04 18:50:15 -0700

    grid layout and added header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,20 @@
 import './App.css'
 import './App-mobile.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import PageHeader from './components/PageHeader.jsx'
+
 
 
 
 const App = () => {
-  const envelope = <FontAwesomeIcon icon={faEnvelope} />
-  const linkedIn = <FontAwesomeIcon icon={faLinkedin} />
-  const git = <FontAwesomeIcon icon={faGithub} />
+
 
   return(
     <>
-      <div className='body'>
-        <section className="pageLeft">
-          <h1>Chris Frey</h1>
-          <h3>Web Developer and Software Engineer</h3>
-          <h5>I build things for people, and the web.</h5>
+    <PageHeader />
 
-          <ul className="contact">
-            <li><a className="button" href="mailto:chris.frey@hey.com">{envelope}</a></li>
-            <li><a className="button" href="https://www.linkedin.com/in/chris-l-frey/" target='_blank'>{linkedIn}</a></li>
-            <li><a className="button" href="https://github.com/Chris-Frey" target='_blank'>{git}</a></li>
-            <li><a className="button" href="">Projects</a></li>
-          </ul>
+
+          <div className='body'>
+        <section className="pageLeft">
 
           <h3>About Me</h3>
           <div className='aboutMe'>
---
+++ src/components/PageHeader.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+
+const PageHeader = () => {
+  const envelope = <FontAwesomeIcon icon={faEnvelope} />
+  const linkedIn = <FontAwesomeIcon icon={faLinkedin} />
+  const git = <FontAwesomeIcon icon={faGithub} />
+  return(
+    <>
+    <div className='header' id='myHeader'>
+      <h1>Chris Frey</h1>
+      <h3>Web Developer and Software Engineer</h3>
+      <h5>I build things for people, and the web.</h5>
+
+      <ul className="contact">
+        <li><a className="button" href="mailto:chris.frey@hey.com">{envelope}</a></li>
+        <li><a className="button" href="https://www.linkedin.com/in/chris-l-frey/" target='_blank'>{linkedIn}</a></li>
+        <li><a className="button" href="https://github.com/Chris-Frey" target='_blank'>{git}</a></li>
+        <li><a className="button" href="">Projects</a></li>
+      </ul>
+    </div>
+    </>
+  )
+
+}
+
+export default PageHeader
