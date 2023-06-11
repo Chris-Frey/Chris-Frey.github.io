f34f48139020c48bd80b5be5aa165b2d6b7b3ba7
chris frey <chris.frey@live.com>
2023-06-11 10:01:15 -0700

    card styling and freezing project frame

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -15,14 +15,17 @@ const PageHeader = () => {
     <div className='header' id='myHeader'>
       <h1>Chris Frey<h1 class="blinky" >|</h1>
       </h1>
-      <h3>Web Developer and Software Engineer</h3>
+      <h3 className="subHead">Web Developer and Software Engineer</h3>
       <h5>I build things for people, and the web.</h5>
 
       <ul className="contact">
-        <li><a className="button" href="mailto:chris.frey@hey.com">{envelope}</a></li>
-        <li><a className="button" href="https://www.linkedin.com/in/chris-l-frey/" target='_blank'>{linkedIn}</a></li>
-        <li><a className="button" href="https://github.com/Chris-Frey" target='_blank'>{git}</a></li>
-        <li><a className="button" href="">Projects</a></li>
+        <li className="contactButton"><a className="button" href="mailto:chris.frey@hey.com">{envelope}</a></li>
+        <li className="contactButton"><a className="button" href="https://www.linkedin.com/in/chris-l-frey/" target='_blank'>{linkedIn}</a></li>
+        <li className="contactButton"><a className="button" href="https://github.com/Chris-Frey" target='_blank'>{git}</a></li>
+        <div className="technicalSkillsButton">
+          <li className="contactButton"><a className="button" href="">Technical Skills</a></li>
+        </div>
+        {/* <li className="contactButton"><a className="button" href="">Projects</a></li> */}
       </ul>
     </div>
     </>
--- src/components/PageRight.jsx
+++ src/components/PageRight.jsx
@@ -6,13 +6,14 @@ const PageRight = () => {
     <>
       <section className="pageRight">
         <h3 className="projects">PROJECTS</h3>
-        <p>Project 1</p>
-        <img className="projectPic" src="https://picsum.photos/400/200" />
-        <p>Project 2</p>
-        <img className="projectPic" src="https://picsum.photos/400/200" />
-        <p>Project 3</p>
-        <img className="projectPic" src="https://picsum.photos/400/200" />
-
+        <div className="projectBody">
+          <p>Project 1</p>
+          <img className="projectPic" src="https://picsum.photos/400/200" />
+          <p>Project 2</p>
+          <img className="projectPic" src="https://picsum.photos/400/200" />
+          <p>Project 3</p>
+          <img className="projectPic" src="https://picsum.photos/400/200" />
+        </div>
       </section>
     </>
   )
